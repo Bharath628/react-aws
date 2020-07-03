d74e1ae68b40d29de81e57d1aa36073cfb8acf7a
Bharath <bharath628@gmail.com>
2020-07-03 09:28:54 -0400

    testing notification

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Bharath Added this
+          Edit <code>src/App.js</code> and save to reload. Bharath testing Notification
         </p>
         <a
           className="App-link"
